77fbdc46b39c7b2885c301ea7ffc71034f63aa5c
futalis <26900448+futalis@users.noreply.github.com>
2026-04-12 12:48:05 -0700

    Completed leetcode 17

--- 1.HashMap/17-LetterPhoneCombos/solution.cpp
+++ 1.HashMap/17-LetterPhoneCombos/solution.cpp
@@ -3,13 +3,39 @@
 #define CATCH_CONFIG_MAIN
 #include <algorithm>
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 
 #include "catch_amalgamated.hpp"
 
 class Solution {
  public:
-  std::vector<std::string> letterCombinations(std::string digits) {}
+  std::vector<std::string> letterCombinations(std::string digits) {
+    if (digits.empty()) {
+      return {};
+    }
+    std::unordered_map<int, std::vector<char>> phone_letters{
+        {'2', {'a', 'b', 'c'}}, {'3', {'d', 'e', 'f'}},
+        {'4', {'g', 'h', 'i'}}, {'5', {'j', 'k', 'l'}},
+        {'6', {'m', 'n', 'o'}}, {'7', {'p', 'q', 'r', 's'}},
+        {'8', {'t', 'u', 'v'}}, {'9', {'w', 'x', 'y', 'z'}},
+    };
+    std::vector<std::string> curr_layer{""};
+    std::vector<std::string> next_layer;
+    for (char c : digits) {
+      const std::vector<char>& letters = phone_letters[c];
+      const int letters_size = letters.size();
+      next_layer.resize(curr_layer.size() * letters_size);
+      for (int j = 0; j < curr_layer.size(); j++) {
+        for (int i = 0; i < letters_size; i++) {
+          next_layer[i + (letters_size * j)] = curr_layer[j] + letters[i];
+        }
+      }
+      curr_layer.swap(next_layer);
+      next_layer.clear();
+    }
+    return curr_layer;
+  }
 };
 
 TEST_CASE("Letter Combo Examples") {
